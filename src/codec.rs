cf52a9714defbcf90baf2438bd4bff84847f8d9c
arthur-zhang <zhangya_no1@qq.com>
2023-03-22 22:24:28 +0800

    refract
    Signed-off-by: arthur-zhang <zhangya_no1@qq.com>

--- src/codec.rs
+++ src/codec.rs
@@ -318,15 +318,6 @@ impl GetDataResponse {
     }
 }
 
-
-pub fn maybe_read_bool(bytes: &mut bytes::BytesMut) -> bool {
-    return if bytes.remaining() >= 1 {
-        bytes.get_u8() == 1
-    } else {
-        false
-    };
-}
-
 #[derive(Debug, FromPrimitive, ToPrimitive)]
 pub enum XidCodes {
     ConnectXid = 0,
@@ -336,9 +327,7 @@ pub enum XidCodes {
     SetWatchesXid = -8,
 }
 
-
 pub struct ClientPacketCodec;
-
 
 impl ClientPacketCodec {
     pub fn new() -> Self {
@@ -346,94 +335,13 @@ impl ClientPacketCodec {
     }
 }
 
-pub fn ensure_min_length(len: i32, min: i32) -> Result<(), ZkError> {
-    if len < min {
-        return Err(ZkError::InvalidPacketLength(len));
-    }
-    Ok(())
-}
-
 impl Encoder<RequestPacket> for ClientPacketCodec {
     type Error = ZkError;
 
     fn encode(&mut self, item: RequestPacket, dst: &mut bytes::BytesMut) -> Result<(), Self::Error> {
-        println!("size: {}", item.size());
         dst.put_i32(item.size() as i32);
         item.serialize_into(dst)?;
         Ok(())
-    }
-}
-
-impl Decoder for ClientPacketCodec {
-    type Item = ZkResponse;
-    type Error = ZkError;
-
-    fn decode(&mut self, src: &mut bytes::BytesMut) -> Result<Option<Self::Item>, Self::Error> {
-        if src.len() < 4 {
-            return Ok(None);
-        }
-        let packet_len = src.get_i32();
-        println!("packet len: {}", packet_len);
-        if packet_len == 0 {
-            return Ok(None);
-        }
-
-        let xid = src.get_i32();
-        println!("xid: {}", xid);
-        let zxid = src.get_i64();
-        let err = src.get_i32();
-        match xid {
-            -2 => {
-                return Ok(Some(ZkResponse::Ping({
-                    ReplyHeader {
-                        xid,
-                        zxid,
-                        err,
-                    }
-                })));
-            }
-            _ => {}
-        }
-        if err != 0 {
-            return Ok(Some(ZkResponse::Ping({
-                ReplyHeader {
-                    xid,
-                    zxid,
-                    err,
-                }
-            })));
-        }
-
-        println!("err: {}", err);
-        let len = src.get_i32();
-        println!("len: {}", len);
-
-        let mut buf: Vec<u8>;
-        if len > 0 {
-            buf = vec![0; len as usize];
-            src.copy_to_slice(&mut buf);
-        } else {
-            buf = vec![];
-        }
-        Ok(Some(ZkResponse::GetData(GetDataResponse {
-            reply_header: ReplyHeader {
-                xid,
-                zxid,
-                err,
-            },
-            data: buf,
-            czxid: src.get_i64(),
-            mzxid: src.get_i64(),
-            ctime: src.get_i64(),
-            mtime: src.get_i64(),
-            version: src.get_i32(),
-            cversion: src.get_i32(),
-            aversion: src.get_i32(),
-            ephemeral_owner: src.get_i64(),
-            data_length: src.get_i32(),
-            num_children: src.get_i32(),
-            pzxid: src.get_i64(),
-        })))
     }
 }
 
@@ -465,76 +373,6 @@ impl ServerPacketCodec {
         };
         Ok(connect_req)
     }
-}
-
-impl Encoder<ZkResponse> for ServerPacketCodec {
-    type Error = ZkError;
-
-    fn encode(&mut self, item: ZkResponse, dst: &mut bytes::BytesMut) -> Result<(), Self::Error> {
-        dst.put_i32(item.size() as i32);
-        item.serialize_into(dst);
-        Ok(())
-    }
-}
-
-fn ensure_max_len(len: usize) -> Result<(), ZkError> {
-    // if len > MAX_PACKET_LENGTH {
-    //     return Err(ZkError::InvalidPacketLength(len));
-    // }
-    Ok(())
-}
-
-fn get_acl(bytes: &mut bytes::BytesMut) -> Result<Vec<Acl>, ZkError> {
-    let len = bytes.get_i32();
-    if len <= 0 {
-        return Ok(vec![]);
-    }
-    if bytes.len() < len as usize {
-        return Err(ZkError::InvalidPacketLength(len));
-    }
-    ensure_max_len(len as usize)?;
-    let mut vec = vec![];
-    for _ in 0..len {
-        let perms = bytes.get_i32();
-        let scheme = get_data(bytes)?;
-        let cred = get_data(bytes)?;
-        vec.push(Acl {
-            perms,
-            scheme,
-            cred,
-        });
-    }
-    Ok(vec)
-}
-
-fn get_data(bytes: &mut bytes::BytesMut) -> Result<Vec<u8>, ZkError> {
-    let len = bytes.get_i32();
-    println!("get data: len: {}", len);
-    if len <= 0 {
-        return Ok(vec![]);
-    }
-    if bytes.len() < len as usize {
-        return Err(ZkError::InvalidPacketLength(len));
-    }
-    ensure_max_len(len as usize)?;
-    let mut vec = vec![0; len as usize];
-    bytes.copy_to_slice(&mut vec);
-    Ok(vec)
-}
-
-fn get_str(bytes: &mut bytes::BytesMut) -> Result<String, ZkError> {
-    let len = bytes.get_i32();
-    if len <= 0 {
-        return Ok("".to_string());
-    }
-    if bytes.len() < len as usize {
-        return Err(ZkError::InvalidPacketLength(len));
-    }
-    ensure_max_len(len as usize)?;
-    let mut vec = vec![0; len as usize];
-    bytes.copy_to_slice(&mut vec);
-    let str = String::from_utf8(vec).map_err(|e| ZkError::InvalidString)?;
-    Ok(str)
 }
 
 impl Decoder for ServerPacketCodec {
@@ -659,3 +497,79 @@ impl Decoder for ServerPacketCodec {
         unreachable!()
     }
 }
+
+fn ensure_min_length(len: i32, min: i32) -> Result<(), ZkError> {
+    if len < min {
+        return Err(ZkError::InvalidPacketLength(len));
+    }
+    Ok(())
+}
+
+fn ensure_max_len(len: usize) -> Result<(), ZkError> {
+    // if len > MAX_PACKET_LENGTH {
+    //     return Err(ZkError::InvalidPacketLength(len));
+    // }
+    Ok(())
+}
+
+fn get_acl(bytes: &mut bytes::BytesMut) -> Result<Vec<Acl>, ZkError> {
+    let len = bytes.get_i32();
+    if len <= 0 {
+        return Ok(vec![]);
+    }
+    if bytes.len() < len as usize {
+        return Err(ZkError::InvalidPacketLength(len));
+    }
+    ensure_max_len(len as usize)?;
+    let mut vec = vec![];
+    for _ in 0..len {
+        let perms = bytes.get_i32();
+        let scheme = get_data(bytes)?;
+        let cred = get_data(bytes)?;
+        vec.push(Acl {
+            perms,
+            scheme,
+            cred,
+        });
+    }
+    Ok(vec)
+}
+
+fn get_data(bytes: &mut bytes::BytesMut) -> Result<Vec<u8>, ZkError> {
+    let len = bytes.get_i32();
+    println!("get data: len: {}", len);
+    if len <= 0 {
+        return Ok(vec![]);
+    }
+    if bytes.len() < len as usize {
+        return Err(ZkError::InvalidPacketLength(len));
+    }
+    ensure_max_len(len as usize)?;
+    let mut vec = vec![0; len as usize];
+    bytes.copy_to_slice(&mut vec);
+    Ok(vec)
+}
+
+fn get_str(bytes: &mut bytes::BytesMut) -> Result<String, ZkError> {
+    let len = bytes.get_i32();
+    if len <= 0 {
+        return Ok("".to_string());
+    }
+    if bytes.len() < len as usize {
+        return Err(ZkError::InvalidPacketLength(len));
+    }
+    ensure_max_len(len as usize)?;
+    let mut vec = vec![0; len as usize];
+    bytes.copy_to_slice(&mut vec);
+    let str = String::from_utf8(vec).map_err(|e| ZkError::InvalidString)?;
+    Ok(str)
+}
+
+pub fn maybe_read_bool(bytes: &mut bytes::BytesMut) -> bool {
+    return if bytes.remaining() >= 1 {
+        bytes.get_u8() == 1
+    } else {
+        false
+    };
+}
+
